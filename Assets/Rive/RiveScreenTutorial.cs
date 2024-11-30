67acaa578679a5535dd13de26759f007532d6287
yebin <yebinp1926@gmail.com>
2024-12-01 05:30:43 +0900

    Feat: tutorial ui rive - event, input 세팅

--- Assets/Rive/RiveScreenTutorial.cs
+++ Assets/Rive/RiveScreenTutorial.cs
@@ -7,47 +7,32 @@ using UnityEngine.Assertions;
 using UnityEngine.Rendering;
 public class RiveScreenTutorial : MonoBehaviour
 {
-    //Rive Assets
-    public Rive.Asset tutorialUI;
-    public Rive.Asset alertUI;
-    public Rive.Asset ammoUI;
-    public Rive.Asset hpUI;
-
+    public Rive.Asset[] asset_list;
     public CameraEvent cameraEvent = CameraEvent.AfterEverything;
     public Fit fit = Fit.Contain;
     public Alignment alignment = Alignment.Center;
     public event RiveEventDelegate OnRiveEvent;
     public delegate void RiveEventDelegate(ReportedEvent reportedEvent);
 
-    private Rive.RenderQueue m_renderQueue;
-    private Rive.Renderer m_riveRenderer;
-    private CommandBuffer m_commandBuffer;
+    private Rive.RenderQueue[] m_renderQueue = new Rive.RenderQueue[3];
+    private Rive.Renderer[] m_riveRenderer = new Rive.Renderer[3];
+    private CommandBuffer[] m_commandBuffer = new CommandBuffer[3];
 
-    private Rive.File m_file;
+    private Rive.File[] m_file = new Rive.File[3];
+    private Artboard[] m_artboard = new Artboard[3];
+    private StateMachine[] m_stateMachine = new StateMachine[3];
+    private CameraTextureHelper[] m_helper = new CameraTextureHelper[3];
 
-    //Artboards
-    private Artboard m_artboard;
-    private Artboard alertArtboard;
-    private Artboard ammoArtboard;
-    private Artboard hpArtboard;
+    //public StateMachine stateMachine => m_stateMachine; //state machine is changed to being public
 
-    //State Machines
-    private StateMachine m_stateMachine;
-    private StateMachine alertStateMachine;
-    private StateMachine ammoStateMachine;
-    private StateMachine hpStateMachine;
-
-    private CameraTextureHelper m_helper;
-
-    public StateMachine stateMachine => m_stateMachine;
+    public SMINumber alertCount;
+    public SMINumber hp;
+    public SMINumber ammo;
+    public bool[] isTutorialMissionCleared = new bool[4] { false, false, false, false };
 
     //For Fetching Inputs
-    SMITrigger nextNarration;
-    SMIBool mission1Complete;
-    SMIBool mission2Complete1;
-    SMIBool mission2Complete2;
-    SMIBool mission3Complete;
-    public int missionInt = 0;
+    private SMITrigger nextNarration;
+    public int narrationInt = 0;
 
 
     private static bool flipY()
@@ -64,60 +49,77 @@ public class RiveScreenTutorial : MonoBehaviour
 
     void OnGUI()
     {
-        if (m_helper != null && Event.current.type.Equals(EventType.Repaint))
+        for (int i = 0; i < 3; i++)
         {
-            var texture = m_helper.renderTexture;
+            if (m_helper[i] != null && Event.current.type.Equals(EventType.Repaint))
+            {
+                var texture = m_helper[i].renderTexture;
 
-            var width = m_helper.camera.scaledPixelWidth;
-            var height = m_helper.camera.scaledPixelHeight;
+                var width = m_helper[i].camera.scaledPixelWidth;
+                var height = m_helper[i].camera.scaledPixelHeight;
 
-            GUI.DrawTexture(
-                flipY() ? new Rect(0, height, width, -height) : new Rect(0, 0, width, height),
-                texture,
-                ScaleMode.StretchToFill,
-                true
-            );
+                GUI.DrawTexture(
+                    flipY() ? new Rect(0, height, width, -height) : new Rect(0, 0, width, height),
+                    texture,
+                    ScaleMode.StretchToFill,
+                    true
+                );
 
+            }
         }
-
     }
 
     private void Awake()
     {
-        if (tutorialUI != null)
-        {
-            m_file = Rive.File.Load(tutorialUI);
-            m_artboard = m_file.Artboard(0);
-            m_stateMachine = m_artboard?.StateMachine();
-        }
-
         Camera camera = gameObject.GetComponent<Camera>();
         Assert.IsNotNull(camera, "RiveScreen must be attached to a camera.");
 
-        // Make a RenderQueue that doesn't have a backing texture and does not
-        // clear the target (we'll be drawing on top of it).
-        m_renderQueue = new Rive.RenderQueue(null, false);
-        m_riveRenderer = m_renderQueue.Renderer();
-        m_commandBuffer = m_riveRenderer.ToCommandBuffer();
-
-        if (!Rive.RenderQueue.supportsDrawingToScreen())
+        for (int i = 0; i < 3; i++)
         {
-            m_helper = new CameraTextureHelper(camera, m_renderQueue);
-            m_commandBuffer.SetRenderTarget(m_helper.renderTexture);
-        }
-        camera.AddCommandBuffer(cameraEvent, m_commandBuffer);
+            if (asset_list[i] != null)
+            {
+                m_file[i] = Rive.File.Load(asset_list[i]);
+                m_artboard[i] = m_file[i].Artboard(0);
+                m_stateMachine[i] = m_artboard[i]?.StateMachine();
+            }
+            // Make a RenderQueue that doesn't have a backing texture and does not
+            // clear the target (we'll be drawing on top of it).
+            m_renderQueue[i] = new Rive.RenderQueue(null, false);
+            m_riveRenderer[i] = m_renderQueue[i].Renderer();
+            m_commandBuffer[i] = m_riveRenderer[i].ToCommandBuffer();
 
-        DrawRive(m_renderQueue);
+            if (!Rive.RenderQueue.supportsDrawingToScreen())
+            {
+                m_helper[i] = new CameraTextureHelper(camera, m_renderQueue[i]);
+                m_commandBuffer[i].SetRenderTarget(m_helper[i].renderTexture);
+            }
+            camera.AddCommandBuffer(cameraEvent, m_commandBuffer[i]);
+
+            DrawRive(i);
+        }
+        OnRiveEvent += RiveScreen_OnRiveEvent;
+
     }
 
-    void DrawRive(Rive.RenderQueue queue)
+    void DrawRive(int i)
     {
-        if (m_artboard == null || alertArtboard == null || ammoArtboard == null || hpArtboard == null)
+        if (m_artboard[i] == null)
         {
             return;
         }
-        m_riveRenderer.Align(fit, alignment ?? Alignment.Center, m_artboard);
-        m_riveRenderer.Draw(m_artboard);
+        if (i == 1 || i == 2)
+        {
+            m_riveRenderer[i].Align(Fit.None, Alignment.TopRight, m_artboard[i]);
+        }
+        else if (i == 3)
+        {
+            m_riveRenderer[i].Align(Fit.None, Alignment.BottomRight, m_artboard[i]);
+        }
+        else
+        {
+            m_riveRenderer[i].Align(Fit.None, Alignment.TopLeft, m_artboard[i]);
+        }
+        m_riveRenderer[i].Draw(m_artboard[i]);
 
     }
 
@@ -126,79 +128,112 @@ public class RiveScreenTutorial : MonoBehaviour
 
     private void Update()
     {
-        m_helper?.UpdateTextureHelper();
-        if (m_artboard == null)
-        {
-            return;
-        }
+        // m_helper?.UpdateTextureHelper();
+        // if (m_artboard == null)
+        // {
+        //     return;
+        // }
 
         //fetching inputs
-        nextNarration = m_stateMachine.GetTrigger("nextNarration");
-        mission1Complete = m_stateMachine.GetBool("mission1_complete");
-        mission2Complete1 = m_stateMachine.GetBool("mission2_complete1");
-        mission2Complete2 = m_stateMachine.GetBool("mission2_complete2");
-        mission3Complete = m_stateMachine.GetBool("mission3_complete");
+        nextNarration = m_stateMachine[0].GetTrigger("nextNarration");
+        isTutorialMissionCleared[0] = m_stateMachine[0].GetBool("mission1_complete").Value;
+        isTutorialMissionCleared[1] = m_stateMachine[0].GetBool("mission2_complete1").Value;
+        isTutorialMissionCleared[2] = m_stateMachine[0].GetBool("mission2_complete2").Value;
+        isTutorialMissionCleared[3] = m_stateMachine[0].GetBool("mission3_complete").Value;
 
         Camera camera = gameObject.GetComponent<Camera>();
         if (camera != null)
         {
-            Vector3 mousePos = camera.ScreenToViewportPoint(Input.mousePosition);
-            Vector2 mouseRiveScreenPos = new Vector2(
-                mousePos.x * camera.pixelWidth,
-                (1 - mousePos.y) * camera.pixelHeight
-            );
-            if (m_lastMousePosition != mouseRiveScreenPos)
+            for (int i = 0; i < 3; i++)
             {
-                Vector2 local = m_artboard.LocalCoordinate(
-                    mouseRiveScreenPos,
-                    new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
-                    fit,
-                    alignment
+                if (m_artboard[i] == null || m_stateMachine[i] == null /* || m_helper[i] == null*/)
+                {
+                    //Debug.Log("artboard is null for: " + i);
+                    continue; // Skip uninitialized elements
+                }
+                m_helper[i]?.UpdateTextureHelper();
+                if (m_artboard[i] == null) //used to be: if (m_artboard == null)
+                {
+                    return;
+                }
+                Vector3 mousePos = camera.ScreenToViewportPoint(Input.mousePosition);
+                Vector2 mouseRiveScreenPos = new Vector2(
+                    mousePos.x * camera.pixelWidth,
+                    (1 - mousePos.y) * camera.pixelHeight
                 );
-                m_stateMachine?.PointerMove(local);
-                m_lastMousePosition = mouseRiveScreenPos;
+                if (m_lastMousePosition != mouseRiveScreenPos)
+                {
+                    Vector2 local = m_artboard[i].LocalCoordinate(
+                        mouseRiveScreenPos,
+                        new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
+                        fit,
+                        alignment
+                    );
+                    m_stateMachine[i]?.PointerMove(local);
+                    m_lastMousePosition = mouseRiveScreenPos;
+                }
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Vector2 local = m_artboard[i].LocalCoordinate(
+                        mouseRiveScreenPos,
+                        new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
+                        fit,
+                        alignment
+                    );
+                    m_stateMachine[i]?.PointerDown(local);
+                    m_wasMouseDown = true;
+                }
+                else if (m_wasMouseDown)
+                {
+                    m_wasMouseDown = false;
+                    Vector2 local = m_artboard[i].LocalCoordinate(
+                        mouseRiveScreenPos,
+                        new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
+                        fit,
+                        alignment
+                    );
+                    m_stateMachine[i]?.PointerUp(local);
+                }
+                //m_stateMachine[i]?.Advance(Time.deltaTime);
             }
-            if (Input.GetMouseButtonDown(0))
-            {
-                Vector2 local = m_artboard.LocalCoordinate(
-                    mouseRiveScreenPos,
-                    new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
-                    fit,
-                    alignment
-                );
-                m_stateMachine?.PointerDown(local);
-                m_wasMouseDown = true;
-            }
-            else if (m_wasMouseDown)
-            {
-                m_wasMouseDown = false;
-                Vector2 local = m_artboard.LocalCoordinate(
-                    mouseRiveScreenPos,
-                    new Rect(0, 0, camera.pixelWidth, camera.pixelHeight),
-                    fit,
-                    alignment
-                );
-                m_stateMachine?.PointerUp(local);
-            }
+
         }
 
         // Find reported Rive events before calling advance.
-        foreach (var report in m_stateMachine?.ReportedEvents() ?? Enumerable.Empty<ReportedEvent>())
+        for (int i = 0; i < 3; i++)
         {
-            OnRiveEvent?.Invoke(report);
+            foreach (var report in m_stateMachine[i]?.ReportedEvents() ?? Enumerable.Empty<ReportedEvent>())
+            {
+                OnRiveEvent?.Invoke(report);
+                //RiveScreen_OnRiveEvent(report);
+            }
+            m_stateMachine[i]?.Advance(Time.deltaTime);
         }
-
-        m_stateMachine?.Advance(Time.deltaTime);
 
     }
 
     private void OnDisable()
     {
         Camera camera = gameObject.GetComponent<Camera>();
-        if (m_commandBuffer != null && camera != null)
+        for (int i = 0; i < 3; i++)
         {
-            camera.RemoveCommandBuffer(cameraEvent, m_commandBuffer);
+            if (m_commandBuffer[i] != null && camera != null)
+            {
+                camera.RemoveCommandBuffer(cameraEvent, m_commandBuffer[i]);
+            }
         }
 
+
+    }
+
+    private void RiveScreen_OnRiveEvent(ReportedEvent reportedEvent)
+    {
+        //Debug.Log($"Event received, name: \"{reportedEvent.Name}\", secondsDelay: {reportedEvent.SecondsDelay}");
+
+        // Access specific event properties
+        if (reportedEvent.Name == "nextNarrationEvent")
+        {
+            narrationInt++;
+        }
     }
 }
